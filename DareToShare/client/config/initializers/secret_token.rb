e96f3f51a4a36c3bad4c6410ca3995cee06b5a1d
Abhishek Bhagia <abhibhagia89@gmail.com>
2012-09-19 21:24:52 +0530

    Created directory structure for dare to share

---
+++ DareToShare/client/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Client::Application.config.secret_token = '05581c060ab1363efa20b0a801ce6af19a3dce04a0be8414588515618a6c0d1c33233e19541fbb3a7cd5abb9abc8460fb51796b8f236ca8001a525f84329ba21'
